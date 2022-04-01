ab74e604bc55d7fc033de42f73d41e0481267da5
IFAKA <facundoarenas29@gmail.com>
2022-04-01 07:49:06 -0300

    backup

---
+++ components/providers/web3/hooks/useAccount.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import useSWR from "swr"
+
+const adminAddresses = {
+  "0x9ef5995a2abe3de9b3fc30ef7c51529789bbce824acba9117fd360fa019e728a": true
+}
+
+export const handler = (web3, provider) => () => {
+
+  const { data, mutate, ...rest } = useSWR(() =>
+    web3 ? "web3/accounts" : null,
+    async () => {
+      const accounts = await web3.eth.getAccounts()
+      return accounts[0]
+    }
+  )
+
+  useEffect(() => {
+    provider &&
+    provider.on("accountsChanged",
+      accounts => mutate(accounts[0] ?? null)
+    )
+  }, [provider])
+
+  return {
+    data,
+    isAdmin: (
+      data &&
+      adminAddresses[web3.utils.keccak256(data)]) ?? false,
+    mutate,
+    ...rest
+  }
+}
