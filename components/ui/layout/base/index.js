3086e4f078212ac918761d14ac374e9bdc856086
IFAKA <facundoarenas29@gmail.com>
2022-06-02 18:48:37 -0300

    Add favicon and title

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,15 +1,10 @@
 
-import Head from 'next/head'
 import { Web3Provider } from "@components/providers"
 import { Navbar, Footer } from "@components/ui/common"
 
 export default function BaseLayout({children}) {
   return (
     <Web3Provider>
-      <Head>
-        <title>Growledge</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <div className="max-w-7xl mx-auto px-4">
         <Navbar />
         <div className="fit">
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,31 @@
+import { Hero } from "@components/ui/common";
+import { CourseList, CourseCard } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+import Head from "next/head";
 
-import { Hero } from "@components/ui/common"
-import { CourseList, CourseCard } from "@components/ui/course"
-import { BaseLayout } from "@components/ui/layout"
-import { getAllCourses } from "@content/courses/fetcher"
-
-export default function Home({courses}) {
+export default function Home({ courses }) {
   return (
     <>
+      <Head>
+        <title>Growledge</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Hero />
-      <CourseList
-        courses={courses}
-      >
-      {course =>
-        <CourseCard
-          key={course.id}
-          course={course}
-        />
-      }
+      <CourseList courses={courses}>
+        {(course) => <CourseCard key={course.id} course={course} />}
       </CourseList>
     </>
-  )
+  );
 }
 
 export function getStaticProps() {
-  const { data } = getAllCourses()
+  const { data } = getAllCourses();
   return {
     props: {
-      courses: data
-    }
-  }
+      courses: data,
+    },
+  };
 }
 
-Home.Layout = BaseLayout
+Home.Layout = BaseLayout;
